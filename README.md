adebf1e0309dcd614fb5f3898ab7b96280382dee
Fábio Dias Soares <fabio.soares@blendmobi.com>
2018-01-04 11:15:21 -0200

    POC complete

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # server-restart
 Express server recovery POC
+
+# Usage
+
+## Install packages
+```bash
+$ npm install
+```
+
+## Run server
+```bash
+$ npm start
+```
---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({'extended': true}));
+app.use(bodyParser.json());
+
+const server = app.listen(process.env.PORT, () => {
+  console.log('Server running on port', process.env.PORT);
+});
+
+
+module.exports = server;
+
+(app => {
+  app.get('/', (req, res) => {
+    res.status(200).json(`Application up and responding on port ${process.env.PORT}`);
+  });
+  app.get('/throw', (req, res) => {
+    res.status(200).json('DONE');
+    process.exit(1);
+  });
+})(app);
+
+process.on('exit', code => {
+  let exec = require('child_process').exec;
+  let cmd = `node ${__dirname}/app.js`;
+
+  console.log('Exiting with code', code);
+  console.log('Restarting server and application...');
+  exec(cmd, () => {
+    process.kill();
+  });
+
+});
