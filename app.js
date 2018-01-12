aafee854bcd4a7642c8d7132008505b4c737e9df
desoares <fabio.desoares@gmail.com>
2018-01-12 00:43:37 -0200

    PM2 POC complete and ok!

--- app.js
+++ app.js
@@ -16,12 +16,39 @@ const server = app.listen(process.env.PORT, () => {
 module.exports = server;
 
 (app => {
+  let exec = require('child_process').exec;
+  
   app.get('/', (req, res) => {
     res.status(200).json(`Application up and responding on port ${process.env.PORT}`);
   });
   app.get('/throw', (req, res) => {
     res.status(200).json('DONE');
     process.exit(1);
+  });
+  app.get('/set-engine', (req, res) => {
+    exec('npm install -g pm2', (err, stdout, stderr) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.status(200).json({'std': {'out': stdout, 'err': stderr}});
+    });
+  });
+  app.post('/new', (req, res) => {
+    if (req.body.app) {
+      let app = req.body.app;
+      let cmd = `pm2 start ${app.entry} --name ${app.name}`;
+      exec(cmd, {
+        'cwd': app.cwd
+      },(err, stdout, stderr) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+
+        res.status(200).json({'std': {'out': stdout, 'err': stderr}});
+      });
+    }
   });
 })(app);
 
